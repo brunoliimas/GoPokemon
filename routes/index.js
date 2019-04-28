382ab2bf344011e3401275e58ed6bfec97df3496
Bruno Lima <ibrunoliimas@gmail.com>
2019-04-28 02:11:59 -0300

    Banco de dados com Mongo e NodeJs (fazer melhori

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+  var db = require("../db");
+  var Users = db.Mongoose.model('usercollection', db.UserSchema, 'usercollection');
+  Users.find({}).lean().exec(
+     function (e, docs) {
+        res.render('userlist', { "userlist": docs });
+  });
+});
